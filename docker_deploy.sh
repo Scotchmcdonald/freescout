98bb483a44e1fd436ef55b61109fdef05ad7bae7
Scott McDonald <scott.mcdonald@borealtek.ca>
2025-11-28 20:09:26 +0000

    Fix: Add missing gd extension to Dockerfile and ensure DB reuse prompt appears in config mode

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -314,7 +314,7 @@ RUN curl -sSLf \
         -o /usr/local/bin/install-php-extensions \
         https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions && \
     chmod +x /usr/local/bin/install-php-extensions && \
-    install-php-extensions imap gmp soap intl bcmath
+    install-php-extensions imap gmp soap intl bcmath gd
 USER www-data
 EOF
 
